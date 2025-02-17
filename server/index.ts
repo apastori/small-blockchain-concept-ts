e324bd1e8c7b2ee3c1ee3ea1d84756ea4e86d227
apastori <alfonso.pastorimata@gmail.com>
2025-02-16 23:35:33 -0300

    EspressAPIGETPOSTWorking

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,25 @@
 import express, { Application, Request, Response } from "express"
 import { Blockchain } from "./blockchain/Blockchain"
+import { Data } from "./types/Data"
+import bodyParser from "body-parser"
 
 const app: Application = express()
 const blockchain: Blockchain = new Blockchain()
 
-app.get('/api/blocks', (_req: Request, res: Response) => {
+// Parse JSON request bodie
+app.use(bodyParser.json())
+
+// Parse URL-encoded request bodies
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/api/blocks', (_req: Request, res: Response): void => {
     res.json(blockchain.getChain())
+})
+
+app.post('/api/mine', (req: Request, res: Response) => {
+    const { data }: { data: Data } = req.body
+    blockchain.addBlock({ data })
+    res.send('Success creating Block')
 })
 
 const PORT: Number = 3000
